b44edfe99ae3c679f1a105e9fc07a19211fa3baa
isa <5445946+icortes@users.noreply.github.com>
2021-11-07 11:57:03 -0800

    created skills model

---
+++ models/Skills.js
@@ -0,0 +1,24 @@
+const {Model, DataTypes} = require('sequelize/types');
+
+class Skills extends Model {}
+
+Skills.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'skills',
+    }
+);
