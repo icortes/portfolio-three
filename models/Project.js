e326a9e0e5de1f266e10e6da3da1ef63ab86f766
isa <5445946+icortes@users.noreply.github.com>
2021-11-07 11:38:56 -0800

    created controller files, created project model

---
+++ models/Project.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require('sequelize/types');
+const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        img_path: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        github: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        deployed_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+);
+
+module.exports = Project;
