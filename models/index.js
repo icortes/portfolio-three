53aef566146407280814c91fc0da77632cc512f2
isa <5445946+icortes@users.noreply.github.com>
2021-11-07 15:18:10 -0800

    seeding database half functional

---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Project = require('./Project');
+const Skills = require('./Skills');
+
+module.exports = {Project, Skills};
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,17 @@
 const sequelize = require('../config/connection');
-const { Project, Skills } = require('../models');
+const {Project, Skills} = require('../models');
 
-const projectData = require('')
+const projectData = require('./projectData.json');
+const skillsData = require('./skillsData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    for (const project of projectData) {
+        await Project.create({...project});
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
