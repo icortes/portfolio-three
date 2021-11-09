cc2b372200bee54aa63db8b86cd014eeac614076
isa <5445946+icortes@users.noreply.github.com>
2021-11-09 00:35:36 -0800

    created projects route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,8 +14,22 @@ router.get('/about-me', async (req, res) => {
         const skillsData = await Skills.findAll().catch((err) => {
             res.json(err);
         });
-        const skills = skillsData.map((skill) => skill.get({ plain: true }));
+        const skills = skillsData.map((skill) => skill.get({plain: true}));
         res.render('about-me', {skills});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/projects', async (req, res) => {
+    try {
+        const projectsData = await Project.findAll().catch((err) => {
+            res.json(err);
+        });
+        const projects = projectsData.map((project) =>
+            project.get({plain: true})
+        );
+        res.render('projects', {projects});
     } catch (err) {
         res.status(500).json(err);
     }
